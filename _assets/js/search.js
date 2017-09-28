b608a070cccca829ead13663e246ad71b078be05
Gavin Lloyd <gavin.lloyd@intel.com>
2017-09-27 17:06:34 -0700

    Initial rework of search

---
+++ _assets/js/search.js
@@ -0,0 +1,80 @@
+$(() => {
+  let $searchBox = $('#searchBox').focus();
+  let $accordion = $('#accordion');
+  let $findSensor = $('#findSensor');
+  let $searchResults = $findSensor.children('.content');
+  let $pageNavigation = $('.page_navigation');
+
+  let facetsTemplate = Handlebars.compile($('#accordianTemplate').html());
+  let searchResultsTemplate = Handlebars.compile($('#findSensorDetail').html());
+
+  /**
+   * Get facets which define the filterable attributes
+   *
+   * @return {Promise<Array>}
+   */
+  let getFacetsData = () => {
+    return $.getJSON('{{ site.baseurl }}/assets/content/facets.json?_={{site.data.global.ajaxVersion}}');
+  };
+
+  /**
+   * Get the combined sensor data (concatenated from individual sensor JSON files)
+   *
+   * @return {Promise<Array>}
+   */
+  let getSensorDetail = () => {
+    return $.getJSON('{{ site.baseurl }}/assets/content/sensorDetail.json?_={{site.data.global.ajaxVersion}}');
+  };
+
+  /**
+   * Render filter selection sidebar
+   *
+   * @return {Promise<jQuery>}
+   */
+  let renderFacets = () => {
+    return getFacetsData().then(data => {
+      $accordion.html(facetsTemplate(data));
+      $accordion.find('.facet-filter').on('change', e => {
+        // filter was changed, update results
+        console.log(e);
+      });
+    });
+  };
+
+  /**
+   * Render a set of search results
+   *
+   * @param {Array} [results] - subset of items from getSensorDetail
+   * @return {jQuery}
+   */
+  let renderSearchResults = (results = []) => {
+    $searchResults.empty();
+    var sensorList = [];
+
+    if (results.length) {
+      $searchResults.html(searchResultsTemplate(results));
+      showPagination();
+    } else {
+      $searchResults.html('<div class="no-results">No sensors match the selected filters.</div>');
+      hidePagination();
+    }
+
+    return $$searchResults;
+  };
+
+  let showPagination = () => {
+    $pageNavigation.show();
+    $findSensor.pajinate({
+        num_page_links_to_display : 10,
+        items_per_page : 10
+    });
+  };
+
+  let hidePagination = () => {
+    $pageNavigation.hide();
+  };
+
+  renderFacets();
+  getSensorDetail().then(renderSearchResults); // show all sensors by default
+
+});
