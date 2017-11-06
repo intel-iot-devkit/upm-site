ad7296ff54f7d8e8e2279593cde54291bf75d863
Gavin Lloyd <gavin.lloyd@intel.com>
2017-11-06 12:02:29 -0800

    Replace tabs in code samples for readability

--- _assets/js/sensorDetail.js
+++ _assets/js/sensorDetail.js
@@ -36,32 +36,39 @@ let initCarousel = () => {
   $('.carousel').carousel({ interval: 2000 });
 };
 
+/**
+ * Fetch a code sample
+ *
+ * @param {String} lang - sample programming language
+ * @param {String} filename
+ * @return {Promise<String>}
+ */
+let fetchCodeSample = (lang, filename) => {
+  return $.get({
+    url: `{{site.data.global.codeSnippetUrl}}/${lang}/${filename}`
+  }).then(res => {
+
+    // strip comments
+    let code = lang === 'python' ?
+      res.replace(/(#.*?\n){4,}/g, '') :
+      res.replace(/\/\*[^]*?\*\//g, ''); // CXX, Java, JavaScript
+
+    return code.replace(/^[\s]*$\n/m, '').replace(/\t/g, '  ');
+  });
+};
+
 let initCodeSamples = () => {
   $('#codeSection li').each((i, el) => {
     let $el = $(el);
 
-    let language = $el.attr('data-parent');
+    let lang = $el.attr('data-parent');
     let filename = $el.attr('data-value');
 
-    let $code = $(`#codeSection [role='tabpanel'][data-parent='${language}'] code`);
+    let $code = $(`#codeSection [role='tabpanel'][data-parent='${lang}'] code`);
 
-    return fetchCodeSample(language, filename).then(code => {
+    return fetchCodeSample(lang, filename).then(code => {
       $code.text(code);
       hljs.highlightBlock($code[0]);
     });
-  });
-};
-
-let fetchCodeSample = (language, filename) => {
-  return $.get({
-    url: `{{site.data.global.codeSnippetUrl}}/${language}/${filename}`
-  }).then(res => {
-
-    // strip comments
-    let code = language === 'python' ?
-      res.replace(/(#.*?\n){4,}/g,'') :
-      res.replace(/\/\*[^]*?\*\//g,''); // CXX, Java, JavaScript
-
-    return code.replace(/^[\s]*$\n/m,'');
   });
 };
